e6039270fcfbc8bc06b3b2c62b1a6dbe820834f5
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-24 08:16:49 +0800

    metadata

--- analysis/01-metadata.R
+++ analysis/01-metadata.R
@@ -12,6 +12,9 @@ filepath <- 'data/metadata/血小板预后2020年11月V1chijh.xlsx'
 
 # Load data ---------------------------------------------------------------
 
-metadata <- readxl::read_excel(path = filepath, sheet = 6)
+metadata <- readxl::read_excel(path = filepath, sheet = 6, skip = 1)
+
+metadata %>% 
+  dplyr::select(barcode, patient_ID, oc, time_on_diagnosis, end_of_chemotherapy, platinum_sensitivity, palindromia, palindromia2, time_of_palindromia, time_of_palindromia2, pfs, alive_type, alive_type2, time_of_die, time_of_followup, os)
 
 
