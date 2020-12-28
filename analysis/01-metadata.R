2bdff6a52e4f5c359a18c4da3d85978ce3ed3f1e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-29 03:56:48 +0800

    metadata stat

--- analysis/01-metadata.R
+++ analysis/01-metadata.R
@@ -20,13 +20,40 @@ metadata <- readxl::read_excel(path = filepath, sheet = 6, skip = 1, col_types =
 
 
 metadata %>% 
-  dplyr::select(barcode, patient_ID, oc, time_on_diagnosis, end_of_chemotherapy, platinum_sensitivity, palindromia, palindromia2, time_of_palindromia, time_of_palindromia2, pfs, alive_type, alive_type2, time_of_die, time_of_followup, os) ->
+  dplyr::select(barcode, patient_ID, oc, time_on_diagnosis, end_of_chemotherapy, platinum_sensitivity, palindromia, palindromia2, time_of_palindromia, time_of_palindromia2, pfs, alive_type, alive_type2, time_of_die, time_of_followup, os) %>% 
+  dplyr::select(barcode, patient_ID, oc, time_on_diagnosis, platinum_sensitivity, palindromia = palindromia2, pfs, alive_type, os) %>% 
+  dplyr::filter(!is.na(time_on_diagnosis)) ->
   metadata
 
+# save metadata
+readr::write_rds(metadata, file = 'data/metadata/metadata.rds.gz', compress = 'gz')
+
+# Platinum ----------------------------------------------------------------
+
 metadata %>% 
-  dplyr::filter(!is.na(time_on_diagnosis))
+  dplyr::select(barcode, patient_ID, oc, platinum_sensitivity) %>% 
+  dplyr::filter(platinum_sensitivity != 'NA') %>% 
+  dplyr::mutate(platinum_sensitivity = ifelse(platinum_sensitivity == 1, 'sensitive', 'resistant')) %>% 
+  dplyr::mutate(platinum_sensitivity = factor(x = platinum_sensitivity, levels = c('sensitive', 'resistant'))) ->
+  metadata_platinum
 
 
-metadata
+# PFS ---------------------------------------------------------------------
+metadata %>% 
+  dplyr::select(barcode, patient_ID, oc, palindromia, pfs) %>% 
+  dplyr::filter(!is.na(pfs)) %>% 
+  dplyr::filter(palindromia != 'NA') %>% 
+  dplyr::mutate(palindromia = ifelse(palindromia == '1', 1, 0)) ->
+  metadata_pfs
+
+# OS ----------------------------------------------------------------------
+metadata %>% 
+  dplyr::select(barcode, patient_ID, oc, status = alive_type, os) %>% 
+  dplyr::filter(status != 'NA') %>% 
+  dplyr::mutate(status = ifelse(status == 'alive', 0, 1)) ->
+  metadata_os
+
+
+
 
 
