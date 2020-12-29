dec45c007f0031898eea2ebf38ef58845d53d54f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2020-12-29 09:04:46 +0800

    filter samples

--- analysis/03-platinum-normalize.R
+++ analysis/03-platinum-normalize.R
@@ -15,16 +15,23 @@ source(file = 'src/doparallel.R', local = TRUE)
 
 # Load data ---------------------------------------------------------------
 
-total315.platinum.se <- readr::read_rds(file = 'data/rda/total315.platinum.se.rds.gz')
+total351.platinum.se <- readr::read_rds(file = 'data/rda/total351.platinum.se.rds.gz')
 
 
 # Function ----------------------------------------------------------------
 
 
+# For filter
+fn_filter_samples <- function(.se) {
+  .m <- apply(X = assay(.se), MARGIN = 2, FUN = function(x) {sum(x) >= 8 * 2e6})
+  .se[, .m]
+}
+
 # Normalize ---------------------------------------------------------------
 
-
-
+fn_parallel_start(n_cores = 50)
+# filter sample with total library size.
+total351.platinum.se.fs <- fn_filter_samples(.se = total351.platinum.se)
 
 
 # Save image --------------------------------------------------------------
