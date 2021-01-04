029b0183caeca368679588a4737b54f16e6f8764
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-05 03:02:55 +0800

    prepare pfs os

--- analysis/06-prepare-pfs-os.R
+++ analysis/06-prepare-pfs-os.R
@@ -2,3 +2,12 @@
 # Library -----------------------------------------------------------------
 
 library(magrittr)
+library(DESeq2)
+
+# Load data ---------------------------------------------------------------
+
+metadata_os <- readr::read_rds(file = 'data/rda/metadata-os.rds.gz') %>% 
+  dplyr::rename(event = status, duration = os)
+metadata_pfs <- readr::read_rds(file = 'data/rda/metadata-pfs.rds.gz') %>% 
+  dplyr::rename(event = palindromia, duration = pfs)
+total800.se <- readr::read_rds(file = 'data/rda/total800.se.rds.gz')
