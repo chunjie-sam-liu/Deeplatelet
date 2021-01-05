21c41a4b2b20d73d2afc16f2707807c66cf9d632
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-06 02:11:05 +0800

    os pfs normalize

--- analysis/07-os-pfs-normalize.R
+++ analysis/07-os-pfs-normalize.R
@@ -26,7 +26,6 @@ fn_filter_samples <- function(.se) {
 fn_check_se <- function(.se) {
   assert_that(!missing(.se) && is(.se, "SummarizedExperiment"), msg = '.se must be provided with SummarizedExperiment.')
   assert_that(identical(colnames(assay(.se)), rownames(colData(.se))), msg = 'colnames(assay(data)) must be equal to rownames(colData(data))')
-  assert_that(has_name(colData(.se), 'platinum'), msg = 'platinum should be in colData')
   
   assert_that(!any(is.infinite(assay(.se))) && !any(is.na(assay(.se))), msg = 'Inf and NA not allowed in .se.')
   assert_that(!any(assay(.se) < 0), msg = 'Negative value not allowed in .se.')
@@ -48,16 +47,13 @@ fn_filter_genes_deseq_normalize <- function(.se, minCounts = 10, fSample = 0.9, 
   message(glue::glue("Notice: {nrow(.se) - nrow(.se_filter_genes)} genes filtered by minimun counts 10 and fraction of samples 0.9. {nrow(.se_filter_genes)} genes remained."))
   
   # Using inequality to filter hypervariants
-  classes <- .se_filter_genes@colData$platinum
-  levels(classes) %>%
-    purrr::map(.f = function(.x) {
-      assay(.se_filter_genes)[, classes == .x] %>%
-        apply(MARGIN = 1, FUN = ineq::ineq, type = 'var') %>%
-        tibble::enframe()
-    }) %>%
-    purrr::reduce(.f = dplyr::left_join, by = 'name') %>%
-    dplyr::filter_if(.predicate = is.numeric, .vars_predicate = dplyr::all_vars(. < thres)) %>%
-    dplyr::pull(name) ->
+  # classes <- .se_filter_genes@colData$platinum
+  
+  assay(.se_filter_genes) %>% 
+    apply(MARGIN = 1, FUN = ineq::ineq, type = 'var') %>% 
+    tibble::enframe() %>% 
+    dplyr::filter(value < thres) %>% 
+    dplyr::pull(name) -> 
     keep_genes
   .se_filter_genes_ineq <- .se_filter_genes[keep_genes, ]
   message(glue::glue("Notice: {nrow(.se_filter_genes) - nrow(.se_filter_genes_ineq)} hypervariant genes filtered. {nrow(.se_filter_genes_ineq)} genes remained."))
@@ -75,19 +71,13 @@ fn_filter_samples_by_cor <- function(.se, coef = 0.4, type = 'spearman') {
   .se_mat <- assay(.se)
   .total_number_sample <- ncol(.se_mat)
   
-  classes <- .se@colData$platinum
-  levels(classes) %>%
-    purrr::map(.f = function(.x) {
-      .se_mat_x <- .se_mat[, classes == .x]
-      .cor <- bigcor(x = .se_mat_x, fun = 'cor', size = floor(ncol(.se_mat_x) / 2), method = type, verbose = FALSE)
-      .corr <- abs(.cor[1:nrow(.cor), 1:ncol(.cor)])
-      .rem_samp <- colnames(.se_mat_x)[rowMeans(.corr) > coef]
-      assert_that(length(.rem_samp) > 0, msg = 'One group has 0 sample remain.')
-      message(glue::glue('Notice: {length(.rem_samp)} samples remained in group {.x}.'))
-      .rem_samp
-    }) %>%
-    purrr::reduce(.f = c) ->
-    .remain_samples
+  .cor <- bigcor(x = .se_mat, fun = 'cor', size = floor(ncol(.se_mat) / 2), method = type, verbose = FALSE)
+  .corr <- abs(.cor[1:nrow(.cor), 1:ncol(.cor)])
+  .rem_samp <- colnames(.se_mat)[rowMeans(.corr) > coef]
+  assert_that(length(.rem_samp) > 0, msg = 'One group has 0 sample remain.')
+  message(glue::glue('Notice: {length(.rem_samp)} samples remained in group.'))
+  
+  .remain_samples <- .rem_samp
   
   message(glue::glue('Notice: {.total_number_sample - length(.remain_samples)} filtered. {length(.remain_samples)} samples remained.'))
   .se[, .remain_samples]
@@ -95,17 +85,17 @@ fn_filter_samples_by_cor <- function(.se, coef = 0.4, type = 'spearman') {
 
 fn_remove_unwanted_variables <- function(.se, .vars = c('oc', 'age', 'lib.size', 'library'), .th_pval = 0.05, .th_var_strong = 0.1) {
   
-  .vars <- if (! 'platinum' %in% .vars) c(.vars, 'platinum') else .vars
+  # .vars <- if (! 'platinum' %in% .vars) c(.vars, 'platinum') else .vars
   # create model and null model
-  .mod <-  model.matrix(~platinum, data = .se@colData)
+  .mod <-  model.matrix(~class, data = .se@colData)
   .mod0 <-  model.matrix(~1, data = .se@colData)
-  .svobj <- sva(dat = assay(.se), mod = .mod, mod0 = .mod0, n.sv = 100)
+  .svobj <- sva(dat = assay(.se), mod = .mod, mod0 = .mod0, n.sv = 50)
   
   # confounding factors with surrogate variables
   # remove the factors that were identified as potential confounding variables from the dataset
   .matrix_w_corr_vars <- foreach(i = 1:ncol(.svobj$sv), .combine = rbind, .packages = c('magrittr')) %dopar% {
     .vars %>% purrr::map_dbl(.f = function(.x) {
-      if (.x %in% c('platinum', 'oc', 'library')) {
+      if (.x %in% c('class', 'oc', 'library')) {
         aov(formula = .svobj$sv[, i] ~ .se@colData[, .x]) %>%
           broom::tidy() %>% dplyr::slice(1) %>% dplyr::pull(p.value)
       } else {
@@ -118,10 +108,10 @@ fn_remove_unwanted_variables <- function(.se, .vars = c('oc', 'age', 'lib.size',
     }) -> .vars_pval
     names(.vars_pval) <- .vars
     
-    if (.vars_pval['platinum'] > .th_pval) {
+    if (.vars_pval['class'] > .th_pval) {
       .strong_var <- names(sort(x = .vars_pval))[1]
     } else {
-      .strong_var <- 'platinum'
+      .strong_var <- 'class'
     }
     if (.vars_pval[.strong_var] < .th_var_strong) {
       .verdict <- names(.vars_pval[.strong_var])
