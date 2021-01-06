c23965646012ba70330a9a0a4729f75cc8c5aa5b
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-07 03:44:44 +0800

    loss surv

--- analysis/08-AESurv.py
+++ analysis/08-AESurv.py
@@ -96,7 +96,7 @@ class LossAELogHaz(nn.Module):
 
     def forward(self, phi, decoded, target_loghaz, target_ae):
         idx_durations, events = target_loghaz
-        loss_surv = (self.loss_surv(phi, idx_durations, events),)
+        loss_surv = self.loss_surv(phi, idx_durations, events)
         loss_ae = self.loss_ae(decoded, target_ae)
         return self.alpha * loss_surv + (1 - self.alpha) * loss_ae
 
