a08c52a6a033a20f95976e36a51d4626f5a69485
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-06 13:52:29 +0800

    add ae surv model

--- analysis/08-AESurv.py
+++ analysis/08-AESurv.py
@@ -14,6 +14,7 @@ from sklearn_pandas import DataFrameMapper
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.nn.modules.linear import Linear
 import torchtuples as tt
 
 from pycox.models import LogisticHazard
@@ -29,12 +30,162 @@ np.random.seed(1234)
 _ = torch.manual_seed(1234)
 
 
-def run():
-    pass
+def load_data(filepath):
+    df = feather.read_dataframe(source=filepath)
+    df_train = df.loc[df.oc == "OC521"].drop(columns=["barcode", "oc"], axis=1)
+    df_val = df.loc[df.oc == "OC44"].drop(columns=["barcode", "oc"], axis=1)
+    df_test1 = df.loc[df.oc == "OC79"].drop(columns=["barcode", "oc"], axis=1)
+    df_test2 = df.loc[df.oc == "OC172"].drop(columns=["barcode", "oc"], axis=1)
+    return df_train, df_val, df_test1, df_test2
+
+
+def get_target(df):
+    return (df["duration"].values, df["event"].values)
+
+
+def transform_features(df_train, df_val, df_test1, df_test2):
+    columns = df_train.columns
+    columns = columns[: len(columns) - 2]
+    standardize = [([col], StandardScaler()) for col in columns]
+
+    x_mapper = DataFrameMapper(standardize)
+
+    x_train = x_mapper.fit_transform(df_train).astype("float32")
+    x_val = x_mapper.transform(df_val).astype("float32")
+    x_test1 = x_mapper.transform(df_test1).astype("float32")
+    x_test2 = x_mapper.transform(df_test2).astype("float32")
+
+    return x_train, x_val, x_test1, x_test2
+
+
+def transform_labels(df_train, df_val, nd=10):
+    num_durations = nd
+    labtrans = LogisticHazard.label_transform(num_durations)
+    y_train_surv = labtrans.fit_transform(*get_target(df_train))
+    y_val_surv = labtrans.transform(*get_target(df_val))
+
+    return y_train_surv, y_val_surv, labtrans
+
+
+class AESurv(nn.Module):
+    def __init__(self, in_features, encoded_features, out_features):
+        super().__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, encoded_features),
+        )
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(encoded_features, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, out_features),
+        )
+
+        # Survival
+        self.survnet = nn.Sequential(
+            nn.Linear(encoded_features, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Linear(32, out_features),
+        )
+
+    def forward(self, input):
+        encoded = self.encoder(input)
+        decoded = self.decoder(encoded)
+        phi = self.survnet(encoded)
+        return phi, decoded
+
+    def predict(self, input):
+        encoded = self.encoder(input)
+        return self.survnet(encoded)
+
+
+class LossAELogHaz(nn.Module):
+    def __init__(self, alpha):
+        super().__init__()
+        assert (alpha >= 0) and (alpha < 1), "Need `alpha` in [0, 1]"
+
+        self.alpha = alpha
+        self.loss_surv = NLLLogistiHazardLoss()
+        self.loss_ae = nn.MSELoss()
+
+    def forward(self, phi, decoded, target_loghaz, target_ae):
+        idx_durations, events = target_loghaz
+        loss_surv = (self.loss_surv(phi, idx_durations, events),)
+        loss_ae = self.loss_ae(decoded, target_ae)
+        return self.alpha * loss_surv + (1 - self.alpha) * loss_ae
+
+
+def run(filepath):
+    # load data
+    df_train, df_val, df_test1, df_test2 = load_data(filepath)
+    # transform features
+    x_train, x_val, x_test1, x_test2 = transform_features(df_train, df_val, df_test1, df_test2)
+    # transform labels
+    y_train_surv, y_val_surv, labtrans = transform_labels(df_train, df_val)
+
+    # make train and validation datasets with tuplefy
+    train = tt.tuplefy(x_train, (y_train_surv, x_train))
+    val = tt.tuplefy(x_val, (y_val_surv, x_val))
+
+    # set arch
+    in_features = x_train.shape[1]
+    encoded_features = 64
+    out_features = labtrans.out_features
+    aesurv = AESurv(in_features, encoded_features, out_features)
+    print(aesurv)
+
+    # loss
+    loss = LossAELogHaz(0.6)
+
+    # model
+    model = LogisticHazard(net=aesurv, optimizer=tt.optim.Adam(0.01), duration_index=labtrans.cuts, loss=loss)
+
+    # metrics
+    metrics = dict(loss_surv=LossAELogHaz(1), loss_ae=LossAELogHaz(0))
+
+    # callbacks
+    callbacks = [tt.cb.EarlyStopping()]
+
+    # cycling
+    batch_size = 10
+    epochs = 1000
+
+    # trainning model
+    log = model.fit(
+        *train, batch_size=batch_size, epochs=epochs, callbacks=callbacks, verbose=False, val_data=val, metrics=metrics
+    )
+
+    res = model.log.to_pandas()
+    print(res.head())
+    _ = res[["train_loss", "val_loss"]].plot()
 
 
 def main():
-    run()
+    run(filepath="data/rda/total416.os.se.norm.feather")
 
 
 if __name__ == "__main__":
