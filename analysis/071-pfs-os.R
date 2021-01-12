2c65ae2142e983c9033d16eaa0a39bef477e4122
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-01-13 07:11:45 +0800

    pfs os

--- analysis/071-pfs-os.R
+++ analysis/071-pfs-os.R
@@ -12,12 +12,14 @@ source(file='src/doparallel.R', local = TRUE)
 
 # Load data ---------------------------------------------------------------
 
-total416.os.se <- readr::read_rds(file = 'data/rda/total416.os.se.rds.gz')
-total434.pfs.se <- readr::read_rds(file = 'data/rda/total434.pfs.se.rds.gz')
+total416.os.se <- readr::read_rds(file = 'data/rda/total416.os.se.norm.rds.gz')
+total434.pfs.se <- readr::read_rds(file = 'data/rda/total434.pfs.se.norm.rds.gz')
 
 
 # Function ----------------------------------------------------------------
-
+fn_get_duration_event <- function(.se) {
+  .se@colData[,c('duration', 'event')]
+}
 
 # Analysis ----------------------------------------------------------------
 
