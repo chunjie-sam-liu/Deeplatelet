48c41331fd6799326cefba42bab0a8887168bfdb
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-02-11 09:13:30 +0800

    pfs os plot

--- analysis/071-pfs-os.R
+++ analysis/071-pfs-os.R
@@ -17,10 +17,12 @@ total434.pfs.se <- readr::read_rds(file = 'data/rda/total434.pfs.se.norm.rds.gz'
 
 
 # Function ----------------------------------------------------------------
-fn_get_duration_event <- function(.se) {
+fn_get_duration_event <- function(.se, .type='os') {
+  .cutoff <- ifelse(.type == 'os', 100, 60)
   .se@colData[,c('barcode', 'oc', 'duration', 'event')] %>% 
     as.data.frame() %>% 
-    tibble::as_tibble()
+    tibble::as_tibble() %>% 
+    dplyr::mutate(duration = ifelse(duration > .cutoff, .cutoff, duration))
 }
 
 fn_surv_plot <- function(.df, .ylab = 'Overall survival probability', .title = 'Overall survival data distribution') {
@@ -63,7 +65,7 @@ fn_surv_plot <- function(.df, .ylab = 'Overall survival probability', .title = '
 # OS ----------------------------------------------------------------------
 
 
-fn_get_duration_event(.se = total416.os.se) %>% 
+fn_get_duration_event(.se = total416.os.se, .type = 'os') %>% 
   fn_surv_plot(.ylab = 'Overall survival probability', .title = 'Overall survival data distribution') -> os.plot
 
 ggsave(
@@ -76,32 +78,32 @@ ggsave(
 )
 
 # set cutoff for durations as 100 months.
-
-total416.os.se.duration <- total416.os.se
-total416.os.se.duration$duration <- ifelse(total416.os.se.duration$duration > 100, 100, total416.os.se.duration$duration)
-
-
-readr::write_rds(x= total416.os.se.duration, file = 'data/rda/total416.os.se.duration.norm.rds.gz')
-
-
-fn_get_duration_event(.se = total416.os.se.duration) %>% 
-  fn_surv_plot(.ylab = 'Overall survival probability', .title = 'Overall survival data distribution') -> os.duration.plot
-
-ggsave(
-  filename ='os-duration-plot.pdf',
-  plot = print(os.duration.plot, newpage = FALSE),
-  device = 'pdf',
-  path = 'data/output',
-  width = 8,
-  height = 9
-)
+# 
+# total416.os.se.duration <- total416.os.se
+# total416.os.se.duration$duration <- ifelse(total416.os.se.duration$duration > 100, 100, total416.os.se.duration$duration)
+# 
+# 
+# readr::write_rds(x= total416.os.se.duration, file = 'data/rda/total416.os.se.duration.norm.rds.gz')
+# 
+# 
+# fn_get_duration_event(.se = total416.os.se.duration) %>% 
+#   fn_surv_plot(.ylab = 'Overall survival probability', .title = 'Overall survival data distribution') -> os.duration.plot
+# 
+# ggsave(
+#   filename ='os-duration-plot.pdf',
+#   plot = print(os.duration.plot, newpage = FALSE),
+#   device = 'pdf',
+#   path = 'data/output',
+#   width = 8,
+#   height = 9
+# )
 
 
 # PFS ---------------------------------------------------------------------
 
 
 
-fn_get_duration_event(.se = total434.pfs.se) %>% 
+fn_get_duration_event(.se = total434.pfs.se, .type = 'pfs') %>% 
   fn_surv_plot(.ylab = "Progression free survival probability", .title='Progression free survival data distribution') -> pfs.plot
 
 ggsave(
@@ -112,24 +114,24 @@ ggsave(
   width = 8,
   height = 9
 )
-
-total434.pfs.se.duration <- total434.pfs.se
-total434.pfs.se.duration$duration <- ifelse(total434.pfs.se.duration$duration > 60, 60, total434.pfs.se.duration$duration)
-
-# write rds for durations
-readr::write_rds(x= total434.pfs.se.duration, file = 'data/rda/total434.pfs.se.duration.norm.rds.gz')
-
-fn_get_duration_event(.se = total434.pfs.se.duration) %>% 
-  fn_surv_plot(.ylab = "Progression free survival probability", .title='Progression free survival data distribution') -> pfs.duration.plot
-
-ggsave(
-  filename ='pfs-duration-plot.pdf',
-  plot = print(pfs.duration.plot, newpage = FALSE),
-  device = 'pdf',
-  path = 'data/output',
-  width = 8,
-  height = 9
-)
+# 
+# total434.pfs.se.duration <- total434.pfs.se
+# total434.pfs.se.duration$duration <- ifelse(total434.pfs.se.duration$duration > 60, 60, total434.pfs.se.duration$duration)
+# 
+# # write rds for durations
+# readr::write_rds(x= total434.pfs.se.duration, file = 'data/rda/total434.pfs.se.duration.norm.rds.gz')
+# 
+# fn_get_duration_event(.se = total434.pfs.se.duration) %>% 
+#   fn_surv_plot(.ylab = "Progression free survival probability", .title='Progression free survival data distribution') -> pfs.duration.plot
+# 
+# ggsave(
+#   filename ='pfs-duration-plot.pdf',
+#   plot = print(pfs.duration.plot, newpage = FALSE),
+#   device = 'pdf',
+#   path = 'data/output',
+#   width = 8,
+#   height = 9
+# )
 
 
 # Save --------------------------------------------------------------------
