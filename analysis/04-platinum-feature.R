f8fc601f51948db99e1ac868e436949f81ad0cca
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-03-22 23:12:21 +0800

    feature eng

--- analysis/04-platinum-feature.R
+++ analysis/04-platinum-feature.R
@@ -42,20 +42,24 @@ fn_select_features <- function(.se) {
   
   .df <- fn_se2df(.se = .train.se[.feats1, ])
   
-  
   set.seed(123)
+
+  fn_parallel_start(n_cores = 50)
+  
   .glm <-  caret::train(
     platinum ~ .,
     data = .df,
     method = "glmnet",
     family = "binomial",
-    trControl = caret::trainControl(method = "cv", number = 10),
+    trControl = caret::trainControl(method = "cv", number = 5),
     tuneGrid =  expand.grid(
       alpha = 1,
       lambda = 10^seq(-3, 3,length = 100)
     ),
-    tuneLenght = 10
+    tuneLength = 5
   )
+  
+  fn_parallel_stop()
   
   coef(.glm$finalModel, .glm$bestTune$lambda) %>%
     as.matrix() %>%
@@ -69,15 +73,9 @@ fn_select_features <- function(.se) {
 
 # Feature selection -------------------------------------------------------
 
-# start parallel
-fn_parallel_start(n_cores = 50)
-# feature selection
-feats <- fn_select_features(.se = total351.platinum.se.norm)
-
-fn_parallel_stop()
-
-readr::write_rds(x = feats, file = 'data/rda/00-selected-features.rds.gz', compress = 'gz')
+panel <- fn_select_features(.se = total351.platinum.se.norm)
+readr::write_rds(x = panel, file = 'data/rda/panel.rds.gz', compress = 'gz')
 
 # Save image --------------------------------------------------------------
 
-save.image(file = 'data/rda/04-1-platinum-feature.rda', ascii = FALSE, compress = TRUE)
+save.image(file = 'data/rda/04-platinum-feature.rda', ascii = FALSE, compress = TRUE)
