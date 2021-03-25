ac08e2c0a39d4d3965ffc7dcaa6e2b264c9a2ec0
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-03-26 03:30:27 +0800

    panels

--- analysis/14-panels.R
+++ analysis/14-panels.R
@@ -36,6 +36,32 @@ ggsave(
 )
 
 
+
+# Duration ----------------------------------------------------------------
+total416.os.se <- readr::read_rds(file = 'data/rda/total416.os.se.norm.rds.gz')
+total434.pfs.se <- readr::read_rds(file = 'data/rda/total434.pfs.se.norm.rds.gz')
+
+total416.os.se@colData %>% 
+  as.data.frame() %>% 
+  dplyr::select(barcode, os_event = event, os_duration = duration) ->
+  os_duration
+
+total434.pfs.se@colData %>% 
+  as.data.frame() %>% 
+  dplyr::select(barcode, pfs_event = event, pfs_duration = duration) ->
+  pfs_duration
+
+
+
+os_duration %>% 
+  dplyr::inner_join(pfs_duration, by = "barcode") %>% 
+  ggplot(aes(x = os_duration, y = pfs_duration)) +
+  geom_smooth()
+
+cor.test(os_pfs_duration$os_duration, os_pfs_duration$pfs_duration)
+
+
+
 # Save image --------------------------------------------------------------
 
 save.image(file = "data/rda/14-panels.rda")
