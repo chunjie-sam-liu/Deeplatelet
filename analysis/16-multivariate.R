745c7549cd8841ae9c57b5d757f40bacc54f0069
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-06-16 11:49:25 +0800

    unicox

--- analysis/16-multivariate.R
+++ analysis/16-multivariate.R
@@ -94,9 +94,49 @@ human_read_latex_pval <- function(.x, .s = NA) {
   
 }
 
+fn_plot_hr <- function(.d) {
+  .d %>% 
+  ggplot(aes(x = hazard_ratio, y = formalname)) +
+    geom_point(size = 3, color = "red", fill = "red", shape = 23) +
+    geom_vline(xintercept = 1, linetype = 5, color = "black", size = 0.5) +
+    geom_errorbarh(aes(xmax = ci.high, xmin = ci.low, height = 0.2), size = 1) +
+    geom_text(aes(x = -8, y = formalname, label = hr_label), size = 6, hjust = 0.5) +
+    geom_text(aes(x = 35, y = formalname, label = pval_label), size = 6, hjust = 1) +
+    scale_y_discrete(expand = c(0.2, 0)) +
+    scale_x_continuous(expand = c(0.18, 0, 0.05, 0)) +
+    theme(
+      panel.grid = element_blank(),
+      panel.background = element_blank(),
+      axis.line.x = element_line(colour = "black"),
+      axis.ticks.y = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text = element_text(size = 14, colour = "black"),
+      axis.text.y = element_text(size = 16),
+      axis.title.x = element_text(size = 20),
+      plot.title = element_text(size = 20, hjust = 0.3)
+    ) +
+    labs(x = "Hazard ratio") +
+    annotate(geom = "text", x = -8, y = 8, label = "HR (95% CI)", size = 6, vjust = 1) +
+    annotate(geom = "text", x = 35, y = 8, label = "P value", size = 6, vjust = 1, hjust = 1)
+}
+
+fn_unicox <- function(.group, .data) {
+  
+  coxph(formula = Surv(duration, event) ~ rlang::eval_bare(rlang::sym(.group)), data = .data) %>% 
+    broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>% 
+    dplyr::select(
+      term,
+      hazard_ratio = estimate,
+      ci.low = conf.low,
+      ci.high = conf.high,
+      pval = p.value
+    )
+}
+
 # Multi-Cox ---------------------------------------------------------------
 
 # OS ----------------------------------------------------------------------
+
 os_risk_group %>% 
   dplyr::select(barcode, oc, stage, CA125, age, platelet_count, platinum, riskscore, riskscore_group = group, residual, event, duration) ->
   os_risk_group_s
@@ -115,24 +155,75 @@ ggsave(
   height = 3.5
 )
 
-os_risk_group_s %>% 
+os_risk_group_s %>%
+  tidyr::drop
   dplyr::mutate(stage_group = factor(stage, levels = c("E", "L"))) %>% 
   dplyr::mutate(ca125_group = factor(ifelse(CA125 > 35, "CA125>35", "CA125<=35"), levels = c("CA125<=35", "CA125>35"))) %>% 
   dplyr::mutate(age_group = factor(ifelse(age > 50, "age>50", "age<=50"), levels = c("age<=50", "age>50"))) %>% 
   dplyr::mutate(plc_group = factor(ifelse(platelet_count > 350, "plc>350", "plc<=350"), levels = c("plc<=350", "plc>350"))) %>% 
   dplyr::mutate(platinum_group = factor(platinum, levels = c("sensitive", "resistant"), ordered = FALSE)) %>% 
-  dplyr::mutate(residual_group = factor(residual, levels = c("non-R0", "R0"))) %>% 
+  dplyr::mutate(residual_group = factor(residual, levels = c("R0", "non-R0"))) %>% 
   dplyr::mutate(riskscore_group = factor(riskscore_group, levels = c("Low", "High")))->
   os_risk_group_s_s
 
+# Unicox ------------------------------------------------------------------
+unicox_df <- 
+  tibble::tibble(group = c(
+    "riskscore_group",
+    "ca125_group",
+    "age_group",
+    "plc_group",
+    "stage_group",
+    "residual_group",
+    "platinum_group"
+  ), formalname = c(
+    "Risk score (high vs low)",
+    "CA125 (>35 vs <=35)",
+    "Age (>50 vs <=50)",
+    "PLC (>350 vs <= 350)",
+    "Stage (late vs early)",
+    "Debulking (suboptimal vs optimal)",
+    "Platinum (resitant vs senstive)"
+  ))
+
+unicox_df %>% 
+  dplyr::mutate(unicox = purrr::map(.x = group, .f = fn_unicox, .data = os_risk_group_s_s)) %>% 
+  tidyr::unnest(cols = c(unicox)) %>% 
+  dplyr::select(term = group, hazard_ratio, ci.low, ci.high, pval, formalname) %>% 
+  dplyr::arrange(-hazard_ratio) %>% 
+  dplyr::mutate(formalname = factor(formalname, levels = formalname)) ->
+  os_unicox_reg;os_unicox_reg
+
+os_unicox_reg %>% 
+  dplyr::mutate(hr_label = glue::glue("{round(hazard_ratio, 2)}({round(ci.low, 2)}-{round(ci.high, 2)})")) %>% 
+  dplyr::mutate(pval_label = signif(pval, 2)) %>% 
+  fn_plot_hr() +
+  labs(title = "OS UniCox HR") ->
+  os_hr_ucox_plot;os_hr_ucox_plot
+
+ggsave(
+  filename = "OS-HR-UCox.pdf",
+  plot = os_hr_ucox_plot,
+  device = "pdf",
+  path = "data/newoutput",
+  width = 10,
+  height = 4
+)
+
+# Multicox ----------------------------------------------------------------
+
 coxph(formula = Surv(duration, event) ~ 
         riskscore_group + 
+        ca125_group +
         age_group + 
         plc_group + 
         stage_group +
         residual_group +
         platinum_group, 
-      data = os_risk_group_s_s) %>% 
+      data = os_risk_group_s_s) ->
+  os.coxph
+
+os.coxph %>% 
   broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>% 
   dplyr::select(
     term,
@@ -143,6 +234,7 @@ coxph(formula = Surv(duration, event) ~
   ) %>% 
   dplyr::mutate(formalname = c(
     "Risk score (high vs low)",
+    "CA125 (>35 vs <=35)",
     "Age (>50 vs <=50)",
     "PLC (>350 vs <= 350)",
     "Stage (late vs early)",
@@ -151,36 +243,17 @@ coxph(formula = Surv(duration, event) ~
     )) %>% 
   dplyr::arrange(-hazard_ratio) %>% 
   dplyr::mutate(formalname = factor(formalname, levels = formalname)) ->
-  os_multicox_reg
+  os_multicox_reg;os_multicox_reg
 
 writexl::write_xlsx(os_multicox_reg, path = "data/newoutput/OS-multicox-reg.xlsx")
 
 os_multicox_reg %>% 
   dplyr::mutate(hr_label = glue::glue("{round(hazard_ratio, 2)}({round(ci.low, 2)}-{round(ci.high, 2)})")) %>% 
   dplyr::mutate(pval_label = signif(pval, 2)) %>% 
-  ggplot(aes(x = hazard_ratio, y = formalname)) +
-  geom_point(size = 3, color = "red", fill = "red", shape = 23) +
-  geom_vline(xintercept = 1, linetype = 5, color = "black", size = 0.5) +
-  geom_errorbarh(aes(xmax = ci.high, xmin = ci.low, height = 0.2), size = 1) +
-  geom_text(aes(x = -8, y = formalname, label = hr_label), size = 6, hjust = 0.5) +
-  geom_text(aes(x = 28, y = formalname, label = pval_label), size = 6, hjust = 1) +
-  scale_y_discrete(expand = c(0.2, 0)) +
-  scale_x_continuous(expand = c(0.18, 0, 0.05, 0)) +
-  theme(
-    panel.grid = element_blank(),
-    panel.background = element_blank(),
-    axis.line.x = element_line(colour = "black"),
-    axis.ticks.y = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text = element_text(size = 14, colour = "black"),
-    axis.text.y = element_text(size = 16),
-    axis.title.x = element_text(size = 20),
-    plot.title = element_text(size = 20, hjust = 0.3)
-  ) +
-  labs(x = "Hazard ratio", title = "OS HR") +
-  annotate(geom = "text", x = -8, y = 7, label = "HR (95% CI)", size = 6, vjust = 1) +
-  annotate(geom = "text", x = 28, y = 7, label = "P value", size = 6, vjust = 1, hjust = 1) ->
+  fn_plot_hr() +
+  labs(title = "OS MultiCox HR") ->
   os_hr_mcox_plot;os_hr_mcox_plot
+
 ggsave(
   filename = "OS-HR-MCox.pdf",
   plot = os_hr_mcox_plot,
@@ -189,6 +262,10 @@ ggsave(
   width = 10,
   height = 4
 )
+
+
+
+
 
 
 # PFS ---------------------------------------------------------------------
@@ -217,20 +294,26 @@ pfs_risk_group_s %>%
   dplyr::mutate(stage_group = factor(stage, levels = c("E", "L"))) %>% 
   dplyr::mutate(ca125_group = factor(ifelse(CA125 > 35, "CA125>35", "CA125<=35"), levels = c("CA125<=35", "CA125>35"))) %>% 
   dplyr::mutate(age_group = factor(ifelse(age > 50, "age>50", "age<=50"), levels = c("age<=50", "age>50"))) %>% 
-  dplyr::mutate(plc_group = factor(ifelse(platelet_count > 350, "plc>350", "plc<=350"), levels = c("plc<=350", "plc>350"))) %>% 
+  dplyr::mutate(plc_group = factor(ifelse(platelet_count > 300, "plc>350", "plc<=350"), levels = c("plc<=350", "plc>350"))) %>% 
   dplyr::mutate(platinum_group = factor(platinum, levels = c("sensitive", "resistant"), ordered = FALSE)) %>% 
-  dplyr::mutate(residual_group = factor(residual, levels = c("non-R0", "R0"))) %>% 
+  dplyr::mutate(residual_group = factor(residual, levels = c("R0", "non-R0"))) %>% 
   dplyr::mutate(riskscore_group = factor(riskscore_group, levels = c("Low", "High")))->
   pfs_risk_group_s_s
 
 coxph(formula = Surv(duration, event) ~ 
         riskscore_group + 
+        ca125_group +
         age_group + 
         plc_group + 
         stage_group +
         residual_group +
         platinum_group, 
-      data = pfs_risk_group_s_s) %>% 
+      data = pfs_risk_group_s_s) ->
+  pfs.coxph
+
+step(pfs.coxph)
+
+pfs.coxph %>% 
   broom::tidy(exponentiate = TRUE, conf.int = TRUE) %>% 
   dplyr::select(
     term,
@@ -241,6 +324,7 @@ coxph(formula = Surv(duration, event) ~
   ) %>% 
   dplyr::mutate(formalname = c(
     "Risk score (high vs low)",
+    "CA125 (>35 vs <=35)",
     "Age (>50 vs <=50)",
     "PLC (>350 vs <= 350)",
     "Stage (late vs early)",
@@ -256,29 +340,10 @@ writexl::write_xlsx(pfs_multicox_reg, path = "data/newoutput/PFS-multicox-reg.xl
 pfs_multicox_reg %>% 
   dplyr::mutate(hr_label = glue::glue("{round(hazard_ratio, 2)}({round(ci.low, 2)}-{round(ci.high, 2)})")) %>% 
   dplyr::mutate(pval_label = signif(pval, 2)) %>% 
-  ggplot(aes(x = hazard_ratio, y = formalname)) +
-  geom_point(size = 3, color = "red", fill = "red", shape = 23) +
-  geom_vline(xintercept = 1, linetype = 5, color = "black", size = 0.5) +
-  geom_errorbarh(aes(xmax = ci.high, xmin = ci.low, height = 0.2), size = 1) +
-  geom_text(aes(x = -8, y = formalname, label = hr_label), size = 6, hjust = 0.5) +
-  geom_text(aes(x = 35, y = formalname, label = pval_label), size = 6, hjust = 1) +
-  scale_y_discrete(expand = c(0.2, 0)) +
-  scale_x_continuous(expand = c(0.18, 0, 0.05, 0)) +
-  theme(
-    panel.grid = element_blank(),
-    panel.background = element_blank(),
-    axis.line.x = element_line(colour = "black"),
-    axis.ticks.y = element_blank(),
-    axis.title.y = element_blank(),
-    axis.text = element_text(size = 14, colour = "black"),
-    axis.text.y = element_text(size = 16),
-    axis.title.x = element_text(size = 20),
-    plot.title = element_text(size = 20, hjust = 0.3)
-  ) +
-  labs(x = "Hazard ratio", title = "PFS HR") +
-  annotate(geom = "text", x = -8, y = 7, label = "HR (95% CI)", size = 6, vjust = 1) +
-  annotate(geom = "text", x = 35, y = 7, label = "P value", size = 6, vjust = 1, hjust = 1) ->
+  fn_plot_hr() +
+  labs(title = "PFS HR") ->
   pfs_hr_mcox_plot;pfs_hr_mcox_plot
+
 ggsave(
   filename = "PFS-HR-MCox.pdf",
   plot = pfs_hr_mcox_plot,
@@ -287,3 +352,36 @@ ggsave(
   width = 10,
   height = 4
 )
+
+
+
+# Unicox ------------------------------------------------------------------
+
+
+unicox_df %>% 
+  dplyr::mutate(unicox = purrr::map(.x = group, .f = fn_unicox, .data = os_risk_group_s_s)) %>% 
+  tidyr::unnest(cols = c(unicox)) %>% 
+  dplyr::select(term = group, hazard_ratio, ci.low, ci.high, pval, formalname) %>% 
+  dplyr::arrange(-hazard_ratio) %>% 
+  dplyr::mutate(formalname = factor(formalname, levels = formalname)) ->
+  os_unicox_reg;os_unicox_reg
+
+os_unicox_reg %>% 
+  dplyr::mutate(hr_label = glue::glue("{round(hazard_ratio, 2)}({round(ci.low, 2)}-{round(ci.high, 2)})")) %>% 
+  dplyr::mutate(pval_label = signif(pval, 2)) %>% 
+  fn_plot_hr() +
+  labs(title = "OS UniCox HR") ->
+  os_hr_ucox_plot;os_hr_ucox_plot
+
+ggsave(
+  filename = "OS-HR-UCox.pdf",
+  plot = os_hr_ucox_plot,
+  device = "pdf",
+  path = "data/newoutput",
+  width = 10,
+  height = 4
+)
+
+# Save image --------------------------------------------------------------
+
+save.image('data/rda/16-multivariate.rda')
