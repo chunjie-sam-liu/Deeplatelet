a03516cd5beb4e37e0ee912367ecdb2acc419901
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-08-29 20:45:53 +0800

    doparallel

--- src/doparallel.R
+++ src/doparallel.R
@@ -4,15 +4,16 @@ fn_parallel_start <- function(n_cores = 10) {
   # https://github.com/ljdursi/beyond-single-core-R
   # detailed information for R parallel computing.
   n_detected_cores <- parallel::detectCores()
-  
+
   assert_that(n_detected_cores > n_cores, msg = glue::glue("The n_core {n_cores} exceeds the total number of cores {n_detected_cores}."))
   print(glue::glue("Notice: Running process in parallel with {n_cores} cores."))
-  
+
   global_cluster <<- parallel::makeForkCluster(nnodes = n_cores)
-  
+
   doParallel::registerDoParallel(cl = global_cluster)
   print(glue::glue("Notice: {foreach::getDoParWorkers()} processes in background."))
 }
+
 
 # Stop
 fn_parallel_stop <- function() {
